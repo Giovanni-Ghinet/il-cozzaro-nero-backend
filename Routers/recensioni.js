01cf78ea85beb000f174964201718ca22a973c36
mauriziopalmisano <maurizio.palmisano1@gmail.com>
2026-06-10 18:22:26 +0200

    creazione della rotta store

--- Routers/recensioni.js
+++ Routers/recensioni.js
@@ -1,5 +1,6 @@
 import express, { request, response } from 'express'
 import idValidator from '../Middlewears/idValidator.js';
+import { store } from '../Controllers/controllerMaurizio.js';
 
 
 
@@ -9,12 +10,12 @@ const recensioni = express.Router();
 
 
 
-recensioni.get('/', index);
-recensioni.get('/:id', idValidator, show);
+// recensioni.get('/', index);
+// recensioni.get('/:id', idValidator, show);
 recensioni.post('/', store);
-recensioni.put('/:id', idValidator, update);
-recensioni.patch('/:id', idValidator, modify);
-recensioni.delete('/:id', idValidator, destroy);
+// recensioni.put('/:id', idValidator, update);
+// recensioni.patch('/:id', idValidator, modify);
+// recensioni.delete('/:id', idValidator, destroy);
 
 
 
