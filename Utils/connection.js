52d9c51a14e48b56fe6d0e0114ce97198407b022
mauriziopalmisano <maurizio.palmisano1@gmail.com>
2026-06-10 13:31:43 +0200

    creato file di connessione al DB

---
+++ Utils/connection.js
@@ -0,0 +1,12 @@
+import { createConnection } from "mysql2/promise";
+
+
+const config = {
+    host : process.env.DB_HOSTNAME,
+    port : process.env.DB_PORT,
+    user : process.env.DB_USERNAME,
+    password : process.env.DB_PASSWORD,
+    database : process.env.DB_DATABASE
+};
+
+export const connection = await createConnection(config);
