2f8a65d2c27618202da5836aba30062e954934bc
mauriziopalmisano <maurizio.palmisano1@gmail.com>
2026-06-11 17:17:00 +0200

    creata funzione per normalizzare i dati

--- Controllers/productController.js
+++ Controllers/productController.js
@@ -1,20 +1,33 @@
 import { connection } from '../Utils/connection.js';
+import { normalizingData } from '../Utils/function.js';
 
 
 export const index = async (request, response) => {
     try{
-        const [rows] = await connection.execute('SELECT * FROM products');
+        const queryProductIndex = `
+            select p.name, p.description, p.price, p.country, p.image, p.availability, p.created_at,r.id as id_review, r.author, r.title, r.text, r.valutation, c.name as category
+            from products p
+                join reviews r
+                    on r.id_product = p.id
+                join product_category pt
+                    on pt.id_product = p.id
+                join categories c
+                    on c.id = pt.id_category
+            order by created_at;
+        `;
+        const [rows] = await connection.execute(queryProductIndex);
+        const productList = normalizingData(rows);
         response
             .json({
-                errro: null,
-                result:rows
+                error: null,
+                result:productList
             });
     }catch(error){
         response
             .status(500)
             .json({
                 error: "errore nella connessione",
-                message: null
+                result: null
             });
     }
 };
@@ -28,7 +41,7 @@ export const show = async (request, response) => {
                 .status(404)
                 .json({
                     error: 'Prodotto non trovato',
-                    message: null
+                    result: null
                 });
         }
         response
@@ -41,7 +54,7 @@ export const show = async (request, response) => {
             .status(500)
             .json({
                 error: "errore nella connessione",
-                message: null
+                result: null
             });
     }
 };
--- Utils/function.js
+++ Utils/function.js
@@ -30,8 +30,8 @@ const valueValidator = (obj) => {
 
     if (typeof title !== 'string' || title.trim() === '' || title.length > 200) return false;
     if (typeof text !== 'string' || text.trim() === '') return false;
-    if (typeof author !== 'string' || author.trim() === ''|| author.length > 200) return false;
-    if (typeof product !== 'string' || product.trim() === ''|| product.length > 200) return false;
+    if (typeof author !== 'string' || author.trim() === '' || author.length > 200) return false;
+    if (typeof product !== 'string' || product.trim() === '' || product.length > 200) return false;
     if (typeof valutation !== 'number' || valutation < 0 || valutation > 5 || Number.isNaN(valutation)) return false;
 
     return true;
@@ -40,4 +40,37 @@ const valueValidator = (obj) => {
 export const validator = (obj) => {
     if (!keyValidator(obj)) return false;
     return valueValidator(obj);
+}
+
+
+
+
+export function normalizingData(dataArray) {
+    const productsMap = {};
+    const IMAGE_PREFIX = "http://localhost:3000/";
+
+    for (const product of dataArray) {
+
+        const { id_review, author, title, text, valutation, image, price, created_at,  ...productData } = product;
+        const productName = product.name;
+        const priceConverted = Number(price);
+
+        if (!productsMap[productName]) {
+            const formattedDate = created_at ? new Date(created_at).toLocaleDateString('it-IT') : null;
+            productsMap[productName] = {
+                ...productData,
+                created_at:formattedDate,
+                price: priceConverted,
+                image: image ? `${IMAGE_PREFIX}${image}` : "https://placehold.co/600x400/png",
+                reviews: []
+            };
+        }
+
+        if (id_review !== undefined && id_review !== null) {
+            const review = { id_review, author, title, text, valutation };
+            productsMap[productName].reviews.push(review);
+        }
+    }
+
+    return Object.values(productsMap);
 }
